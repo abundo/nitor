25dc0168b108cdacdd575f4fdc565e13b794688c
Anders Lowinger <anders@abundo.se>
2020-12-28 02:54:33 +0000

    Only restart apache2 if both cert files are in place

--- nitor.py
+++ nitor.py
@@ -8,14 +8,21 @@ nitor : to rest, lean, support oneself / trust in, depend upon.
 nitor : brilliance, brightness, glow, elegance, splendor.
 """
 
+import os
 import sys
 import subprocess
 import argparse
+import datetime
 
-# ----- defaults -----
+
+# ----- defaults, start -----
 
 APACHE_SRC_DIR = "/var/lib/nitor"
 APACHE_DST_DIR = "/etc/ssl/nitor"
+
+SSH_USER = "nitor"
+
+# ----- defaults, end -----
 
 
 class Install_Cert:
@@ -30,18 +37,43 @@ class Install_Cert:
         if cert_dst is None:
             cert_dst = APACHE_DST_DIR
 
-        cmd = f"scp -i /root/.ssh/nitor {cert_src} nitor@{hostname}:{cert_dst}"
+        cmd = f"scp -i /root/.ssh/nitor {cert_src} {SSH_USER}@{hostname}:{cert_dst}"
         print(cmd)
         ret = subprocess.run(cmd, shell=True)
         if ret.returncode != 0:
             print("Error:", ret)
+            return
 
-        if cert_src.endswith(".crt"):   # We only restat if both keys are in place
-            cmd = f"ssh -t -i /root/.ssh/nitor nitor@{hostname} sudo /bin/systemctl restart apache2.service"
-            print(cmd)
-            ret = subprocess.run(cmd, shell=True)
-            if ret.returncode != 0:
-                print("Error:", ret)
+        # Compare date between the two certs, on remote host
+        base_name = os.path.basename(cert_src)
+        base_name_noext = os.path.splitext(base_name)[0]
+        file_crt = f"{base_name_noext}.crt"
+        file_key = f"{base_name_noext}.key"
+
+        cmd = f"ssh -i /root/.ssh/nitor {SSH_USER}@{hostname} stat -c %Y {cert_dst}/{file_crt} {cert_dst}/{file_key}"
+        ret = subprocess.run(cmd, shell=True, capture_output=True, universal_newlines=True)
+        if ret.returncode != 0:
+            print("Error:", ret)
+            return
+        lines = ret.stdout.split("\n")
+        if len(lines) != 3:
+            # We did not get two timestamps, both files are not in place on remote host
+            return
+        try:
+            timestamp1 = int(lines[0])
+            timestamp2 = int(lines[1])
+        except ValueError:
+            return
+        diff = datetime.datetime.fromtimestamp(timestamp1) - datetime.datetime.fromtimestamp(timestamp2)
+        if abs(diff.total_seconds()) > 10:
+            # Too large difference, acertmgr has probably not written both files yet
+            return
+
+        cmd = f"ssh -t -i /root/.ssh/nitor {SSH_USER}@{hostname} sudo /bin/systemctl reload apache2.service"
+        print(cmd)
+        ret = subprocess.run(cmd, shell=True)
+        if ret.returncode != 0:
+            print("Error:", ret)
 
     def handle_esxi(self):
         """
@@ -52,7 +84,7 @@ class Install_Cert:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", choices = [
+    parser.add_argument("cmd", choices=[
         "apache2",
         "esxi",
     ])
@@ -79,4 +111,4 @@ def main():
 
 
 if __name__ == "__main__":
-   main()
+    main()
